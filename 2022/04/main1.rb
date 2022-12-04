83521ba76311a412685306d9982c8b9407e982aa
André Durão <andre.durao@wovn.io>
2022-12-04 16:15:03 +0900

    Adds solution to 2022/04 part 1

---
+++ 2022/04/main1.rb
@@ -0,0 +1,15 @@
+total = 0
+
+def range(str)
+  start, finish = str.split('-').map(&:to_i)
+  Array(start..finish)
+end
+
+File.readlines(ARGV[0]).each do |line|
+  l_range, r_range = line.chomp.split(',')
+  l = range(l_range)
+  r = range(r_range)
+  total += 1 if (l - r).empty? || (r - l).empty?
+end
+
+p total
