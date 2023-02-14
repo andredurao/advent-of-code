553ed9b3d1da816bd9f4d3b4bb56472c66e12c4b
André Durão <andre.durao@wovn.io>
2023-02-14 21:16:13 +0900

    13 ex1 just a loop

---
+++ 2022/13/main1.rb
@@ -0,0 +1,27 @@
+require 'set'
+require 'json'
+
+def compare(l, r)
+  sleep 0.5 ; p l ; p r ; p '==='
+  while l.any? && r.any?
+    vl, vr = l.shift, r.shift
+    p [vl, vr]
+    if vl.is_a?(Integer) && vr.is_a?(Integer)
+      return true if vl < vr
+      return false if vl > vr
+    end
+  end
+end
+
+
+items = []
+File.readlines(ARGV[0], chomp: true).each do |line|
+  if !line.empty?
+    items << JSON::parse(line)
+  end
+end
+
+l, r = items.shift(2)
+p "compare: #{compare(l,r)}"
+
+exit 0
