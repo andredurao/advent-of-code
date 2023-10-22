2d2a50c900fbfd3e90ffc2ebc57309b31c79e1fd
Andre Durao <andreluisdurao@gmail.com>
2023-10-22 21:18:42 +0900

    Solution for 2016/3 part2

--- 2016/3/main.go
+++ 2016/3/main.go
@@ -10,6 +10,7 @@ import (
 
 func main() {
 	part1()
+	part2()
 }
 
 func part1() {
@@ -28,6 +29,40 @@ func part1() {
 		}
 		if validTriangle(sides) {
 			count++
+		}
+	}
+	fmt.Println(count)
+}
+
+func part2() {
+	content, _ := os.ReadFile("input")
+	lines := strings.Split(string(content), "\n")
+	trianglesArray := make([][]int, 0)
+	for _, line := range lines {
+		if len(line) == 0 {
+			continue
+		}
+		re := regexp.MustCompile(`\d+`)
+		ss := re.FindAllString(line, -1)
+		sides := make([]int, 3)
+		for i, side := range ss {
+			sides[i], _ = strconv.Atoi(side)
+		}
+		trianglesArray = append(trianglesArray, sides)
+	}
+
+	count := 0
+	for i := 0; i < len(trianglesArray)/3; i++ {
+		row := i * 3
+		for j := 0; j < 3; j++ {
+			triangle := []int{
+				trianglesArray[row][j],
+				trianglesArray[row+1][j],
+				trianglesArray[row+2][j],
+			}
+			if validTriangle(triangle) {
+				count++
+			}
 		}
 	}
 	fmt.Println(count)
