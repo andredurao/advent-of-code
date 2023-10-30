43809478ad13b1455ee297811a5435af838f377f
Andre Durao <andreluisdurao@gmail.com>
2023-10-30 20:02:19 +0900

    Solution for 2016/6 part1

---
+++ 2016/6/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"fmt"
+	"os"
+	"strings"
+)
+
+var lines []string
+
+// usage ./main filename
+func main() {
+	part1()
+}
+
+func part1() {
+	filename := os.Args[1]
+	content, _ := os.ReadFile(filename)
+	lines = strings.Split(string(content), "\n")
+
+	code := ""
+	for i := range lines[0] {
+		charMap := frequencies(lines, i)
+		code = code + string(mostFrequentChar(charMap))
+	}
+	fmt.Println(code)
+}
+
+func frequencies(lines []string, col int) (charMap map[rune]int) {
+	charMap = make(map[rune]int, 0)
+
+	for _, line := range lines {
+		if len(line) == 0 {
+			break
+		}
+		char := rune(line[col])
+		_, found := charMap[char]
+		if found {
+			charMap[char]++
+		} else {
+			charMap[char] = 1
+		}
+	}
+	return
+}
+
+func mostFrequentChar(charMap map[rune]int) rune {
+	max := 0
+	result := '0'
+	for char, count := range charMap {
+		if count > max {
+			result = char
+			max = count
+		}
+	}
+	return result
+}
