31153a1c84874e5bd69f2009b7f30da88ae1fa10
Andre Durao <andreluisdurao@gmail.com>
2023-12-14 14:28:36 -0300

    Solution for 2023/6 part2

--- 2023/6/main.go
+++ 2023/6/main.go
@@ -14,6 +14,7 @@ var lines []string
 func main() {
 	readlines()
 	part1()
+	part2()
 }
 
 func readlines() {
@@ -40,7 +41,34 @@ func part1() {
 		total *= ways
 	}
 	fmt.Println(total)
+}
 
+func part2() {
+	fmt.Println("part 2")
+
+	time := getNumber(lines[0])
+	record := getNumber(lines[1])
+
+	i := 0
+	for {
+		d := getDistance(i, time)
+		if d > record {
+			// as soon as we got the first value we can stop because the
+			// same amount of distances that will be smaller than the record
+			// will also be present on the right side of the values
+			// the values grow as a inverted parabola
+			break
+		}
+		i++
+	}
+
+	// for t=9, if the 1st value to beat the record is 2
+	// there will be also 2 values on the right end that will not reach the record
+	// 0 1 2 3 4 5 6 7 8 9
+	// X X i i i i i i x x
+	res := time - (i * 2) + 1
+
+	fmt.Println(res)
 }
 
 func getDistance(t int, limit int) int {
@@ -55,4 +83,10 @@ func getNumbers(line string) []int {
 		res = append(res, val)
 	}
 	return res
+}
+
+func getNumber(line string) int {
+	re := regexp.MustCompile(`\d+`)
+	val, _ := strconv.Atoi(strings.Join(re.FindAllString(line, -1), ""))
+	return val
 }
