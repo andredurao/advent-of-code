cdfbe41c25b734bc15e64d6f371eae582925a332
Andre Durao <andreluisdurao@gmail.com>
2024-12-02 19:20:30 -0300

    Day 2, part 2

--- 2024/02/main.rb
+++ 2024/02/main.rb
@@ -1,6 +1,6 @@
 raise "usage: main.rb FILENAME" if ARGV.size == 0
 
-class LevelValidator
+class LevelValidator1
   class << self
     def call(line:)
       self.new(line:).valid?
@@ -17,11 +17,11 @@ class LevelValidator
   end
 
   def ordered?
-    @line.sort == @line || @line.sort.reverse == @line
+    line.sort == line || line.sort.reverse == line
   end
 
   def valid_distance?
-    1.upto(@line.size - 1) do |i|
+    1.upto(line.size - 1) do |i|
       dist = (line[i] - line[i-1]).abs
       return false if dist < 1 || dist > 3
     end
@@ -31,7 +31,59 @@ end
 
 total = 0
 File.readlines(ARGV[0]).map(&:chomp).each do |line|
-  total += 1 if LevelValidator.(line:)
+  total += 1 if LevelValidator1.(line:)
 end
 
 puts "part1: #{total}"
+
+# Part 2
+
+class LevelValidator2
+  class << self
+    def call(nums:)
+      self.new(nums:).valid?
+    end
+  end
+
+  attr_accessor :nums
+  def initialize(nums:)
+    @nums = nums
+  end
+
+  def valid?
+    ordered? && valid_distance?
+  end
+
+  def ordered?
+    nums.sort == nums || nums.sort.reverse == nums
+  end
+
+  def valid_distance?
+    1.upto(nums.size - 1) do |i|
+      dist = (nums[i] - nums[i-1]).abs
+      return false if dist < 1 || dist > 3
+    end
+    true
+  end
+end
+
+total = 0
+File.readlines(ARGV[0]).map(&:chomp).each do |line|
+  nums = line.split(' ').map(&:to_i)
+
+  if LevelValidator2.(nums:)
+    total += 1
+  else
+    # brute force removing single itens of array
+    0.upto(nums.size - 1) do |i|
+      partial = nums.dup
+      partial.delete_at(i)
+      if LevelValidator2.(nums: partial)
+        total += 1
+        break
+      end
+    end
+  end
+end
+
+puts "part2: #{total}"
