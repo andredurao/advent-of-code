5cd6431a7b7b532abe41de45be770f62715824a9
Andre Durao <andreluisdurao@gmail.com>
2024-12-03 19:16:48 -0300

    Day 3, part 2

--- 2024/03/main.rb
+++ 2024/03/main.rb
@@ -11,5 +11,21 @@ line.scan(/mul\(\d+\,\d+\)/).each do |cmd|
   total += (m[1].to_i * m[2].to_i)
 end
 
-
 puts "part1: #{total}"
+
+# Part 2
+
+total = 0
+
+token_regex = /mul\(\d+\,\d+\)|do\(\)|don\'t\(\)/
+do_mul = true
+line.scan(token_regex).each do |cmd|
+  do_mul = true if cmd == 'do()'
+  do_mul = false if cmd == "don't()"
+  if cmd.start_with?('mul') && do_mul
+    m = /mul\((\d+)\,(\d+)\)/.match(cmd)
+    total += (m[1].to_i * m[2].to_i)
+  end
+end
+
+puts "part2: #{total}"
