fa23f5d97e8a5ce546b07f5495de8fdd5cdec2a4
Andre Durao <andreluisdurao@gmail.com>
2024-12-08 13:01:32 -0300

    Day 6, part2

--- 2024/06/main.rb
+++ 2024/06/main.rb
@@ -1,19 +1,20 @@
 class Solver
-  attr_reader :grid, :w, :h, :pos, :dir, :steps
+  attr_reader :grid, :w, :h
 
   #       up      right  down   left
   DIRS = [[-1,0], [0,1], [1,0], [0,-1]]
 
   def initialize
+    load_grid
+  end
+
+  def load_grid
     @grid = []
     File.readlines(ARGV[0]).map(&:chomp).each do |line|
       @grid << line.chars
     end
     @h = @grid.size
     @w = @grid[0].size
-    @pos = initial_pos
-    @dir = 0 #up
-    @steps = 0
   end
 
   def valid_pos?(pos)
@@ -26,38 +27,93 @@ class Solver
         return [i,j] if ch == ?^
       end
     end
+    raise 'initial pos could not be found'
   end
 
-  def next_pos
+  def get_next_pos(pos, dir)
     [pos[0]+DIRS[dir][0], pos[1]+DIRS[dir][1]]
   end
 
-  def obstruction?
+  def pos_index(pos)
+    pos[0] * h + pos[1]
+  end
+
+  def obstruction?(pos, dir)
+    next_pos = get_next_pos(pos, dir)
     return false if !valid_pos?(next_pos)
     grid[next_pos[0]][next_pos[1]] == ?#
   end
 
-  def turn
-    @dir = (dir + 1) % 4
+  def turn(dir)
+    (dir + 1) % 4
   end
 
-  def walk
-    turn if obstruction?
+  def walk(pos, dir, steps)
+    dir = turn(dir) if obstruction?(pos, dir)
     if grid[pos[0]][pos[1]] != ?X
-      @steps += 1
       grid[pos[0]][pos[1]] = ?X
+      steps += 1
     end
-    @pos = next_pos
+    [get_next_pos(pos, dir), dir, steps]
   end
 
-  def solve
+  def solve1
+    pos = initial_pos
+    dir = 0 #up
+    res = 0
     while valid_pos?(pos)
-      walk
+      pos, dir, res = walk(pos, dir, res)
     end
-    puts "part1: #{steps}"
+    puts "part1: #{res}"
   end
 
-  def debug
+  def visit(grid, start_pos, dir, visits_map)
+    # debug(pos, dir)
+    visits_map[:level] ||= 0
+    visits_map[:level] += 1
+
+    set = Set.new
+    total = 0
+
+    while true
+      pos = get_next_pos(start_pos, dir)
+
+      return total if !valid_pos?(pos)
+
+      if grid[pos[0]][pos[1]] == ?#
+        dir = turn(dir)
+        next
+      end
+
+      index = pos_index(pos).to_s
+      if visits_map[:level] == 1 && !set.include?(index)
+        grid[pos[0]][pos[1]] = ?#
+        r = visit(grid, start_pos, dir, visits_map.dup)
+        # puts "found: #{pos} #{dir}" if r > 0
+        total += r
+        grid[pos[0]][pos[1]] = ?.
+      end
+
+      start_pos = pos.dup
+      set.add(index)
+
+      # if the index was used twice in the same direction, that's a loop
+      index += ":#{dir}"
+      return 1 if visits_map[index]
+      visits_map[index] = 1
+    end
+  end
+
+  def solve2
+    load_grid
+    og_grid = @grid.map(&:dup)
+    pos = initial_pos
+    r = visit(og_grid, pos, 0, {})
+
+    puts "part2: #{r}"
+  end
+
+  def debug(pos, dir)
     guard_ch = %w[↑ → ↓ ←]
     grid.each_with_index do |row, i|
       str = ''
@@ -72,4 +128,5 @@ end
 raise "usage: main.rb FILENAME" if ARGV.size == 0
 
 solver = Solver.new
-solver.solve
+solver.solve1
+solver.solve2
