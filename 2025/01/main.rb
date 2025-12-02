16169042803170bd188a7d771c6becf6e9daa85f
Andre Durao <andreluisdurao@gmail.com>
2025-12-01 21:01:04 -0300

    Adds part1 method

--- 2025/01/main.rb
+++ 2025/01/main.rb
@@ -1,23 +1,28 @@
 raise "usage: main.rb FILENAME" if ARGV.size == 0
 
-moves = File.readlines(ARGV[0]).map(&:chomp)
-
-res = 0
-pos = 50
-
 def rotate(pos, move)
   dir, qty = move[0], move[1..].to_i
   # R: +, L: -
   pos += qty if dir == ?R
   pos -= qty if dir == ?L
 
-  pos % 100
+  crossed_zero = pos > 99 || pos <= 0
+
+  [pos % 100, crossed_zero]
 end
 
-moves.each do |move|
-  pos, crossed_zero = rotate(pos, move)
-  # p move
-  res += 1 if pos == 0
+def part1(moves)
+  res = 0
+  pos = 50
+
+  moves.each do |move|
+    pos, crossed_zero = rotate(pos, move)
+    res += 1 if pos == 0
+  end
+
+  puts "part1: #{res}"
 end
 
-p res
+moves = File.readlines(ARGV[0]).map(&:chomp)
+
+part1(moves)
