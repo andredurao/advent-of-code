2fb5334c5ad7033bd4a640186cfd0df02f145da4
Andre Durao <andreluisdurao@gmail.com>
2025-12-07 21:45:11 -0300

    2025, Day 5 / part 2

--- 2025/05/main.rb
+++ 2025/05/main.rb
@@ -21,6 +21,34 @@ class Day05
     puts "part1: #{res}"
   end
 
+  def part2
+    # sort ranges
+    @ranges = ranges.sort{|a,b| a.first <=> b.first}
+
+    compacted_ranges = []
+    ranges.each_with_index do |range, i|
+      if i == 0
+        compacted_ranges << range
+      else
+        previous_compacted_range = compacted_ranges.last
+        # check if range is included in previous compacted range
+        if range.first <= previous_compacted_range.last + 1
+          # join ranges
+          v1 = previous_compacted_range.first
+          v2 = [range.last, previous_compacted_range.last].max
+
+          compacted_ranges.pop
+          compacted_ranges << (v1..v2)
+        else
+          compacted_ranges << range
+        end
+      end
+    end
+
+    res = compacted_ranges.reduce(0) {|total, range| total += range.size}
+    puts "part2: #{res}"
+  end
+
   private
 
   def read_file(filename)
@@ -39,3 +67,4 @@ end
 
 app = Day05.new
 app.part1
+app.part2
