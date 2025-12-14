9466bbbe01e53ec658cc8ebf7b61c1b42d0d4061
Andre Durao <andreluisdurao@gmail.com>
2025-12-14 20:47:20 -0300

    2025, Day 10 / part 1

--- 2025/10/main.rb
+++ 2025/10/main.rb
@@ -1,12 +1,14 @@
 require 'chunky_png'
 
 class Machine
-  attr_accessor :lights, :buttons, :joltages
+  attr_accessor :lights, :buttons, :joltages, :lights_base10, :buttons_base10
 
   def initialize(line)
     build_lights(line)
     build_buttons(line)
     build_joltages(line)
+    build_lights_decimal_base10
+    build_buttons_base10
   end
 
   def to_s
@@ -15,6 +17,22 @@ class Machine
   end
 
   private
+
+  def build_lights_decimal_base10
+    binary_str = lights.map{|ch| ch ? 1 : 0}.join
+    @lights_base10 = binary_str.to_i(2)
+  end
+
+  def build_buttons_base10
+    @buttons_base10 = []
+    @buttons.each do |button|
+      binary_value = ?0 * lights.size
+
+      button.each{|index| binary_value[index] = ?1 if index < lights.size}
+
+      @buttons_base10 << binary_value.to_i(2)
+    end
+  end
 
   def build_lights(line)
     @lights = []
@@ -50,8 +68,9 @@ class Day10
 
   def part1
     res = 0
+
     p machines
-    binding.irb
+
     puts "part 1: #{res}"
   end
 
